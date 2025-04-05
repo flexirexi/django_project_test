ebbaaa79dd2a77e03d400b31c6302f38cdf6fcbb
flexirexi <felix_lehmann@ymail.com>
2025-04-05 23:56:57 +0200

    setup of urls.py, views.py, template + static folders

--- hello_world/views.py
+++ hello_world/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, World")
+    return render(request, "hello_world/index.html")
 
 
 def about(request):
